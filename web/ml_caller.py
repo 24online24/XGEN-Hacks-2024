0aee0809bb7343b47ff29234f49100dc410282fc
Rares Chiuzbaian <chiuzbaianrares20@gmail.com>
2024-05-22 00:38:55 +0300

    Fix names

--- web/ml_caller.py
+++ web/ml_caller.py
@@ -5,11 +5,11 @@ from models import News
 
 def predict_real(news: News) -> dict[str, float]:
     model_name_to_function = {
-        "random_forest": fetch_prediction_random_forest,
-        "decision_tree": fetch_prediction_decision_tree,
-        "logistic_regression": fetch_prediction_logistic_regression,
-        "multinomial_nb": fetch_prediction_multinomial_nb,
-        "svm": fetch_prediction_svm
+        "Random Forest": fetch_prediction_random_forest,
+        "Decision Tree": fetch_prediction_decision_tree,
+        "Logistic Regression": fetch_prediction_logistic_regression,
+        "Multinomial Naive Bayes": fetch_prediction_multinomial_nb,
+        "Support Vector Machine": fetch_prediction_svm
     }
 
     with concurrent.futures.ProcessPoolExecutor() as executor:
