b2e0d74617cef5026cf3194c6a6048f80d4ff971
Jacques9 <104220884+Jacques9@users.noreply.github.com>
2024-05-21 19:03:08 +0300

    .

--- ML/random_forest.py
+++ ML/random_forest.py
@@ -3,31 +3,30 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, accuracy_score
-import joblib
+import joblib, re, string
 
-df = pd.read_csv("ML/csv_train/Combined.csv")
+df = pd.read_csv("ML/csv_train/Bigger.csv")
 
-df['combined'] = df['title'] + " " + df['text']
+def preprocess(text):
+    text = text.lower()
+    text = re.sub(r'\[.*?\]', '', text)
+    text = re.sub(r"\\W", " ", text)
+    text = re.sub(r'https?://\S+|www\.\S+', '', text)
+    text = re.sub(r'<.*?>+', '', text)
+    text = re.sub(r'[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub(r'\n', '', text)
+    text = re.sub(r'\w*\d\w*', '', text)
+    return text
 
-texts = df['combined'].tolist()
-labels = df['Label'].tolist()
+df['text'] = df['text'].apply(preprocess)
 
-labels = [int(label) for label in labels]
+x = df["text"]
+y = df["is_fake"]
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-vectorizer = TfidfVectorizer(stop_words='english', max_features=5000)
-X = vectorizer.fit_transform(texts)
-y = labels
+vectorization = TfidfVectorizer()
+x_train = vectorization.fit_transform(x_train)
+x_test = vectorization.transform(x_test)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
-rf_model.fit(X_train, y_train)
-
-y_pred = rf_model.predict(X_test)
-y_prob = rf_model.predict_proba(X_test)
-
-accuracy = accuracy_score(y_test, y_pred)
-report = classification_report(y_test, y_pred)
-
-joblib.dump(rf_model, 'ML/saved_models/random_forest_model.pkl')
-joblib.dump(vectorizer, 'ML/saved_models/tfidf_vectorizer.pkl')
+Random_Forest_Classifier = RandomForestClassifier(random_state=0)
+Random_Forest_Classifier.fit(x_train, y_train)
